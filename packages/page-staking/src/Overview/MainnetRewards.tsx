3e3849525f512f7e5a1f5ea16e87c16ea953318d
shixm1102 <shixm_1102@qq.com>
2022-01-30 10:47:45 +0800

    当前era奖励

--- packages/page-staking/src/Overview/MainnetRewards.tsx
+++ packages/page-staking/src/Overview/MainnetRewards.tsx
@@ -2,16 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 /* eslint-disable */
-import { ActiveEraInfo } from '@polkadot/types/interfaces';
+import { ActiveEraInfo } from "@polkadot/types/interfaces";
 
-import React from 'react';
+import React from "react";
 
-import { useApi, useCall } from '@polkadot/react-hooks';
-import { FormatBalance } from '@polkadot/react-query';
-import BN from 'bn.js';
-import { Icon, Tooltip } from '@polkadot/react-components';
-import { useTranslation } from '@polkadot/apps/translate';
-import { formatBalance } from '@polkadot/util';
+import { useApi, useCall } from "@polkadot/react-hooks";
+import { FormatBalance } from "@polkadot/react-query";
+import BN from "bn.js";
+import { Icon, Tooltip } from "@polkadot/react-components";
+import { useTranslation } from "@polkadot/apps/translate";
+import { formatBalance } from "@polkadot/util";
 
 interface Props {
   children?: React.ReactNode;
@@ -19,45 +19,43 @@ interface Props {
   label?: React.ReactNode;
 }
 
-const UNIT = new BN(1_000_000_000_000)
+const UNIT = new BN(1_000_000_000_000);
 
-function MainnetReward ({ children, className = '', label }: Props): React.ReactElement<Props> {
+function MainnetReward({ children, className = "", label }: Props): React.ReactElement<Props> {
   const { api } = useApi();
   const { t } = useTranslation();
   const activeEraInfo = useCall<ActiveEraInfo>(api.query.staking.activeEra);
 
-  const activeEra = activeEraInfo && (JSON.parse(JSON.stringify(activeEraInfo)).index);
+  const activeEra = activeEraInfo && JSON.parse(JSON.stringify(activeEraInfo)).index;
   const marketPayout = useCall<any>(api.query.staking.erasMarketPayout, [activeEra]);
-  const stakingRewards = new BN(3422.3134898087887).mul(UNIT)
-  const total = marketPayout && stakingRewards.add(new BN(Number(marketPayout).toString()))
+  // const stakingRewards = new BN(3422.3134898087887).mul(UNIT);
+  const stakingRewards = new BN(28333.333333333333).mul(UNIT);
+  const total = marketPayout && stakingRewards.add(new BN(Number(marketPayout).toString()));
 
   return (
     <div className={className}>
-      {label || ''}
-      <FormatBalance
-        value={total}
-        withSi
-        label={
-          <Icon
-            icon='info-circle'
-            tooltip={`mainnet-reward-trigger`}
-          />
-        }
-      >
+      {label || ""}
+      <FormatBalance value={total} withSi label={<Icon icon="info-circle" tooltip={`mainnet-reward-trigger`} />}>
         <Tooltip
           text={
             <>
               <div>
-                <div className='faded'>{t('staking payout: {{stakingRewards}}', {
-                  replace: {
-                    stakingRewards: formatBalance(stakingRewards)
-                  }
-                })}</div>
-                <div className='faded'>{t('market payout: {{marketPayout}}', {
-                  replace: {
-                    marketPayout: marketPayout && formatBalance(new BN(Number(marketPayout).toString()))
-                  }
-                })}</div>
+                <div className="faded">
+                  {" "}
+                  {t("staking payout: {{stakingRewards}}", {
+                    replace: {
+                      stakingRewards: formatBalance(stakingRewards)
+                    }
+                  })}{" "}
+                </div>
+                <div className="faded">
+                  {" "}
+                  {t("market payout: {{marketPayout}}", {
+                    replace: {
+                      marketPayout: marketPayout && formatBalance(new BN(Number(marketPayout).toString()))
+                    }
+                  })}{" "}
+                </div>
               </div>
             </>
           }
